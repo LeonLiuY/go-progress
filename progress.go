6b0ac7635b467bd0d3403acb7cbf37f92e750989
Liu Yang <liu_yang@worksap.co.jp>
2015-07-15 10:48:23 +0800

    Use embedded interface.

--- progress.go
+++ progress.go
@@ -7,14 +7,14 @@ import (
 
 // Wrap an io.Reader instance, add progress information.
 type ProgressReader struct {
-	Input io.Reader
+	io.Reader
 	Size int64
 	Finished int64
 	OnProgress func(float32) //callback function when updating progress
 }
 
 func (c *ProgressReader) Read(p []byte) (n int, err error) {
-    cBytes, err := c.Input.Read(p)
+    cBytes, err := c.Reader.Read(p)
     c.Finished = c.Finished + int64(cBytes)
 	if err == nil && cBytes != 0 && c.OnProgress != nil {
 		c.OnProgress(c.Progress())
@@ -28,14 +28,14 @@ func (c ProgressReader) Progress() (n float32) {
 
 // Wrap an io.Writer instance, add progress information.
 type ProgressWriter struct {
-	Output io.Writer
+	io.Writer
 	Size int64
 	Finished int64
 	OnProgress func(float32) //callback function when updating progress
 }
 
 func (c *ProgressWriter) Write(p []byte) (n int, err error) {
-    cBytes, err := c.Output.Write(p)
+    cBytes, err := c.Writer.Write(p)
     c.Finished = c.Finished + int64(cBytes)
 	if err == nil && cBytes != 0 && c.OnProgress != nil {
 		c.OnProgress(c.Progress())
@@ -49,7 +49,7 @@ func (c ProgressWriter) Progress() (n float32) {
 
 func NewProgressReader(input io.Reader, size int64) *ProgressReader {
 	p := new(ProgressReader)
-	p.Input = input
+	p.Reader = input
 	p.Size = size
 	p.Finished = 0
 	return p
@@ -57,7 +57,7 @@ func NewProgressReader(input io.Reader, size int64) *ProgressReader {
 
 func NewProgressWriter(output io.Writer, size int64) *ProgressWriter {
 	p := new(ProgressWriter)
-	p.Output = output
+	p.Writer = output
 	p.Size = size
 	p.Finished = 0
 	return p
